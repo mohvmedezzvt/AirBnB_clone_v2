cd994d7b255233efef7a9f07b358b7659988beab
devbn3li <fudwhc@gmail.com>
2023-11-23 02:21:15 +0200

    update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
         if key in self.__objects:
             del self.__objects[key]
             self.save()
+
+    def close(self):
+        """Reloads the JSON file to objects"""
+        self.reload()
