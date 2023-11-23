9edcf5206ea33b8fc59d1b48d877aadbbb3ecf7c
devbn3li <fudwhc@gmail.com>
2023-11-23 03:43:02 +0200

    up

--- models/place.py
+++ models/place.py
@@ -1,18 +1,52 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
+from models.review import Review
+from models.amenity import Amenity
 
 
-class Place(BaseModel):
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), 
+           primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), 
+           primary_key=True)
+)
+
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
+    reviews = relationship("Review", backref="place",
+                           cascade="all, delete-orphan")
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    if os.environ['HBNB_ENV'] != 'db':
+        @property
+        def reviews(self):
+            """Getter method to retrieve related Review instances."""
+            from models import storage
+            dic = storage.all('Review')
+            return [].append(v for k, v in dic.items() if self.id == v['place_id'])
+        @property
+        def amenities(self):
+            """Getter method to retrieve related Amenity instances."""
+            from models import storage
+            dic = storage.all('Amenity')
+            return [].append(v for k, v in dic.items() if self.id == v['amenity_ids'])
+        @amenities.setter
+        def amenities(self, obj):
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
