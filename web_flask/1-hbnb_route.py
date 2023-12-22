5cff590ab5e16d6deb8caeb1ccd4d07cadf05fb3
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-23 00:53:35 +0200

    a script that starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module defines a simple Flask application. """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Route handler for the root URL ("/"). """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Route handler for the URL ("/hbnb"). """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
