898f7eefbcde24b87ab3e9546d539a6fd3bd276a
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-29 15:17:06 +0200

    a script that starts a Flask web application

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """ display a HTML page """
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='states')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """ display a HTML page """
+    states = storage.all(State)
+    for state in states.values():
+        if state.id == id:
+            return render_template('9-states.html', state=state, mode='cities')
+    return render_template('9-states.html', state=None, mode='none')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
